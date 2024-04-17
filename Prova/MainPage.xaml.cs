dabade1cbd6429b43b52e3e0a6455c52c4f98505
Coletti <matheus.coletti08@gmail.com>
2024-04-17 17:32:34 -0300

    update resources

---
+++ Prova/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Prova
+{
+    public partial class MainPage : ContentPage
+    {
+        int randomSide = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            SidesPicker.SelectedIndex = 0;
+        }
+        private void CoinButton(object sender, EventArgs e)
+        {
+            
+            Random sideDraw = new Random();
+            randomSide = sideDraw.Next(0, 3);
+            
+            if (randomSide % 2 == 0)
+            {
+                ResultLabel.Text = "Cara";
+                CoinResult.Source = ImageSource.FromFile("cara.png");
+            }
+            else 
+            { 
+                ResultLabel.Text = "Coroa";
+                CoinResult.Source = ImageSource.FromFile("coroa.png");
+            }
+
+        }
+    }
+
+}
